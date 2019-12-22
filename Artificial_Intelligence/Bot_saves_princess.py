aa480299139d7111227712561d0189a70b49ee30
Daniel Gaspar Gonçalves <52671074+d-gaspar@users.noreply.github.com>
2019-12-22 10:34:15 -0300

    Update Bot_saves_princess.py

--- Artificial_Intelligence/Bot_saves_princess.py
+++ Artificial_Intelligence/Bot_saves_princess.py
@@ -11,18 +11,18 @@ def displayPathtoPrincess(M,grid):
         if("p" in value):
             princess = (idx, grid[idx].index("p"))
 
-    direction_h = mario[0] - princess[0]
-    direction_v = mario[1] - princess[1]
+    direction_v = mario[0] - princess[0]
+    direction_h = mario[1] - princess[1]
 
     if(direction_h < 0):
-        output = ["LEFT"]*abs(direction_h)
+        output = ["RIGHT"]*abs(direction_h)
     elif(direction_h > 0):
-        output = ["RIGHT"]*direction_h
+        output = ["LEFT"]*direction_h
 
     if(direction_v < 0):
-        output = output + ["UP"]*abs(direction_v)
+        output = output + ["DOWN"]*abs(direction_v)
     elif(direction_v > 0):
-        output = output + ["DOWN"]*direction_v
+        output = output + ["UP"]*direction_v
 
     return("\n".join(output))
 
