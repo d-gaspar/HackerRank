33bc6a83d2cb8323d0ee0aaae8c1234f71e67193
Daniel Gaspar Gonçalves <52671074+d-gaspar@users.noreply.github.com>
2019-12-22 12:43:07 -0300

    Update BotClean.py

--- Artificial_Intelligence/BotClean.py
+++ Artificial_Intelligence/BotClean.py
@@ -2,43 +2,78 @@
 
 # Head ends here
 
+def update_position(posr, posc, board, move):
+    if(move == "CLEAN"):
+        return()
+    elif(move == "RIGHT"):
+        aux = list(board[posr])
+        aux[posc] = "-"
+        aux[posc+1] = "b"
+        
+        board[posr] = "".join(aux)
+        posc = posc+1
+    elif(move == "LEFT"):
+        aux = list(board[posr])
+        aux[posc] = "-"
+        aux[posc-1] = "b"
+        
+        board[posr] = "".join(aux)
+        posc = posc-1
+    elif(move == "UP"):
+        aux = list(board[posr])
+        aux2 = list(board[posr-1])
+        aux[posc] = "-"
+        aux2[posc] = "b"
+        
+        board[posr] = "".join(aux)
+        board[posr-1] = "".join(aux2)
+        posr = posr - 1
+    elif(move == "DOWN"):
+        aux = list(board[posr])
+        aux2 = list(board[posr+1])
+        aux[posc] = "-"
+        aux2[posc] = "b"
+        
+        board[posr] = "".join(aux)
+        board[posr+1] = "".join(aux2)
+        posr = posr + 1
+        
+    return(posr, posc, board)
+
 def next_move(posr, posc, board):
-    move = ""
-    
     if(board[posr][posc] == "d"):
         return("CLEAN")
     
-    # horizontal (dirty)
-    if(posc == 0):
-        move = "RIGHT"
-        if(board[posr][posc+1] == "d"):
-            return("RIGHT")
-    elif(posc == len(board[0])-1):
-        move = "LEFT"
-        if(board[posr][posc-1] == "d"):
-            return("LEFT")
-    else:
-        move = "RIGHT"
-        if(board[posr][posc+1] == "d"):
-            return("RIGHT")
-        elif(board[posr][posc-1] == "d"):
-            return("LEFT")
+    # find all dirty cell positions
+    dirties_pos = []
+    for idx,value in enumerate(board):
+        if("d" in value):
+            for col in [i for i,j in enumerate(list(value)) if j=="d"]:
+                dirties_pos.append([idx,col])
+    if(len(dirties_pos)==0):
+        return()
     
-    # vertical (dirty)
-    if(posr == 0):
-        if(board[posr+1][posc] == "d"):
-            return("DOWN")
-    elif(posr == len(board)-1):
-        if(board[posr-1][posc] == "d"):
-            return("UP")
-    else:
-        if(board[posr+1][posc] == "d"):
-            return("DOWN")
-        elif(board[posr-1][posc] == "d"):
-            return("UP")
-            move = "UP"
+    # find nearest position
+    min_pos = []
+    min_dis = 100
+    for d in dirties_pos:
+        cur_dis = abs(d[0] - posr) + abs(d[1] - posc)
+        if(cur_dis < min_dis):
+            min_dis = cur_dis
+            min_pos = d
     
-    return(move)
+    direction_h = posc - min_pos[1]
+    direction_v = posr - min_pos[0]
+
+    if(direction_h < 0):
+        return("RIGHT")
+    elif(direction_h > 0):
+        return("LEFT")
+
+    if(direction_v < 0):
+        return("DOWN")
+    elif(direction_v > 0):
+        return("UP")
 
 # Tail starts here
 
@@ -46,3 +81,14 @@ if __name__ == "__main__":
     pos = [int(i) for i in input().strip().split()]
     board = [[j for j in input().strip()] for i in range(5)]
     print(next_move(pos[0], pos[1], board))
+
+# i = 0
+# while("d" in "".join(board)):    
+#     move = next_move(pos[0], pos[1], board)
+#     pos[0], pos[1], board = update_position(pos[0], pos[1], board, move)
+#     print()
+#     print(str(i) + ": " + move)
+#     for row in board:
+#         print(row)
+        
+#     i = i + 1    
